87b2f6a244b13c05c598c5c9854a1bce92f1922b
hayate1234 <hayate140114@gmail.com>
2022-09-09 15:22:47 +0900

    create_table

--- database/migrations/2022_09_08_063152_create_posts_table.php
+++ database/migrations/2022_09_08_063152_create_posts_table.php
@@ -17,8 +17,9 @@ class CreatePostsTable extends Migration
             $table->increments('id');
             $table->string('sake_name',100);
             $table->text('body');
-            $table->increments('good');
+            $table->integer('good');
             $table->string ('image');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
         });
     }
